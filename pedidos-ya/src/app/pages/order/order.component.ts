81684b16ce986388e2997e87842dd15b33352421
Juan Capuano <juancapuano19@gmail.com>
2025-07-02 23:29:09 -0300

    ver perfil - cerrar sesion

--- pedidos-ya/src/app/pages/order/order.component.ts
+++ pedidos-ya/src/app/pages/order/order.component.ts
@@ -7,6 +7,7 @@ import { ApiService } from '../../services/api.service';
 import { OrderService } from '../order.service';
 import { PRODUCTOS, RESTAURANTES } from './mock';
 import { CIUDADES } from './mock';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -21,7 +22,7 @@ export class OrderComponent {
   error = '';
   success = '';
   apiService: any;
-  constructor(private fb: FormBuilder, private orderService : OrderService) {
+  constructor(private fb: FormBuilder, private orderService : OrderService, private router: Router) {
     this.formulario = this.fb.group({
       restaurantId: [null, [ Validators.required, Validators.min(1) ]],
       products: [[], [ Validators.required , Validators.minLength(1) ]],
@@ -50,25 +51,31 @@ export class OrderComponent {
       this.success = '';
       return;
     }
+
+    const token = localStorage.getItem('access_token');
+    if(!token){
+      this.error = 'Tenés que iniciar sesión para registrar una orden';
+      setTimeout(() => this.router.navigate(['/login']), 2000);
+    return;
+    }
+
+    
     try {
       this.error = '';
 
       const raw = this.formulario.value;
 
-    
-      if (typeof raw.products === 'string') {
-       raw.products = raw.products
-         .split(',')
-         .map((id: string) => Number(id.trim()))
-         .filter((id: number) => !isNaN(id));
-    }
+      raw.products = raw.products.map((id: any) => Number(id));
 
       await this.orderService.crearOrden(raw);
       this.formulario.reset();
       this.success = 'Orden registrada exitosamente.';
-    } catch (error: any) {
-      this.error ='Error al registrar la orden';
-      this.success = '';
-    }
+    } 
+   catch (error: any) {
+    console.error('Error capturado:', error);
+    this.error = 'Error al registrar la orden';
+    this.success = '';
+  }
   }
 }
+
